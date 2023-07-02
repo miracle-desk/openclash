63ffd003123399f4b851612a29e88c4bacb7b45a
An** <135781170+miracle-desk@users.noreply.github.com>
2023-07-03 04:31:47 +0700

    Update get-update-provider.py

--- get-update-provider.py
+++ get-update-provider.py
@@ -3,7 +3,7 @@ import yaml
 
 accounts = []  # List untuk menyimpan akun yang telah dikumpulkan
 
-# Perulangan untuk membuka URL sebanyak 10 kali
+# Perulangan untuk membuka URL sebanyak 30 kali
 for _ in range(30):
     url = "https://fool.azurewebsites.net/get?format=clash&mode=cdn&cdn=104.17.3.81&network=ws&arg=xudp,key:value&vpn=trojan,vmess,vless&region=Asia&cc=SG,ID&limit=3&pass=1oqrsj6c"  # Ganti dengan URL yang sesuai
     
